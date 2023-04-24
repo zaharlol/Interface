24792aeef2d67f277b7613a22a838dc763773e63
zaharlol <kovtun.zahar12@mail.ru>
2023-04-24 13:23:57 +0300

    SR

---
+++ SR/Program.cs
@@ -0,0 +1,39 @@
+﻿public interface IPlus
+{
+    int Plus1(int a, int b);
+}
+
+public class Plus : IPlus
+{
+    public int Plus1(int a, int b)
+    {
+        int c = 0;
+        try
+        {
+            c = a + b;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        return c;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        IPlus plus = new Plus();
+        try
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int result = plus.Plus1(a, b);
+            Console.WriteLine(result);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
