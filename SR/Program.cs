c5958c6651a3bf931739b069b6d1511308768bcb
zaharlol <kovtun.zahar12@mail.ru>
2023-04-29 15:04:34 +0300

    Добавили логгер

--- SR/Program.cs
+++ SR/Program.cs
@@ -21,19 +21,65 @@ public class Plus : IPlus
 }
 class Program
 {
+    static ILogger Logger { get; set; }
     static void Main(string[] args)
     {
+        Logger = new Logger();
+        var worker = new Worker1(Logger);
         IPlus plus = new Plus();
         try
         {
+            Console.WriteLine("Введите первое число");
             int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
             int b = Convert.ToInt32(Console.ReadLine());
             int result = plus.Plus1(a, b);
-            Console.WriteLine(result);
+            worker.Work();
+            Console.WriteLine("Результат {0}", result);
         }
         catch (Exception e)
         {
-            Console.WriteLine(e.Message);
+            Logger.Error(e.Message);
+
         }
+        Console.ReadKey();
+    }
+}
+public interface ILogger
+{
+    void Error(string messege);
+    void Event(string messege);
+}
+
+public class Logger : ILogger
+{
+    public void Error(string messege)
+    {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.WriteLine(messege);
+    }
+
+    public void Event(string messege)
+    {
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.WriteLine(messege);
+    }
+}
+public interface IWorker
+{
+    void Work();
+}
+public class Worker1 : IWorker
+{
+    ILogger Logger { get; }
+    public Worker1(ILogger logger)
+    {
+        Logger = logger;
+    }
+    public void Work()
+    {
+        Logger.Event("");
     }
 }
